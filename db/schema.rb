e4c4e9625a6521d5384ce683af15be9fee8314c5
ninhdth-1359 <dao.thi.hai.ninh@sun-asterisk.com>
2023-08-29 15:28:04 +0700

    ninh test

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_27_163428) do
-  create_table "artiles", charset: "utf8mb4", force: :cascade do |t|
+ActiveRecord::Schema.define(version: 2023_08_27_163428) do
+
+  create_table "artiles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.string "description"
   end
 
-  create_table "categories", charset: "utf8mb4", force: :cascade do |t|
+  create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
   end
 
