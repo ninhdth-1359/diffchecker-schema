a9123e86e95de94218e9130eaaa7315e84166d6e
ninhdth-1359 <dao.thi.hai.ninh@sun-asterisk.com>
2023-08-31 14:49:35 +0700

    Delete check schema

--- scripts/diffchecker.sh
+++
@@ -1,9 +0,0 @@
-bundle exec rails db:migrate --trace
-
-if git diff --quiet db/schema.rb; then
-   echo "No significant changes"
-   exit 0
-else
-   echo "There are some significant changes, let's trigger something..."
-   exit 1
-fi
